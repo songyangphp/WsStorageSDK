59e19b726d976c427797bc5d608f5a2449a5efa5
songyang <475511896@qq.com>
2020-10-23 09:30:46 +0800

    songyang commit

--- vendor/epii/admin-ui/src/app/view/common/script.php
+++
@@ -1,2 +0,0 @@
-<script src="<?php echo $_ui_["static_url_pre"]; ?>js/plugins/requirejs/require.js"
-        data-main="<?php echo $_ui_["static_url_pre"]; ?>js/run.js"></script>
--- vendor/epii/tiny-app-plus/gitpush.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git ci -m "init"
-git push origin master
